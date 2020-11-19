28d6364f90ec81f24d23a7c92c9bd76f0948ca43
Clay <clayton_cheung@sfu,.ca>
2020-11-19 01:02:33 -0800

    center home page content

--- main.js
+++ main.js
@@ -6,6 +6,50 @@ window.onload=function(){
             nav1.classList.toggle('show');
         });
 
+
+        var modal = document.getElementById("myModal");
+        var modal2 = document.getElementById("myModal2");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var soundimg = document.getElementById("soundeffect");
+var stopmotionbts = document.getElementById("stopmotionbts");
+var modalImg = document.getElementById("modalimage");
+var captionText = document.getElementById("caption");
+var modalImg2 = document.getElementById("modalimage2");
+var captionText2 = document.getElementById("caption2");
+if(img){
+    img.onclick = function(){
+        modal.style.display = "flex";
+        modalImg.src = this.src;
+        captionText.innerHTML = "Snow Effect in APIC 2018 Reel Project";
+      }
+      soundimg.onclick = function(){
+          modal.style.display = "flex";
+          modalImg.src = this.src;
+          captionText.innerHTML = "Crowd Cheer Sound Effect in APIC 2018 Reel Project";
+
+        }
+        modal.onclick = function() { 
+            modal.style.display = "none";
+          }
+}
+   if(stopmotionbts){
+    stopmotionbts.onclick = function(){
+        modal2.style.display = "flex";
+        modalImg2.src = this.src;
+        captionText2.innerHTML = "Stopmotion Behind the Scene";
+      }
+      modal2.onclick = function() { 
+        modal2.style.display = "none";
+      }
+   }
+   
+
+ 
+
+
+
+
 }
 
 
@@ -15,5 +59,7 @@ function changeIcon(t){
 
 }
 
+
+//   Learned from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_img
 //   Learned from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
 //  Learned from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_like_dislike
